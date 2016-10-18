9005594c14ab6c5ddd1c1ff0ac1a403a313b68a4
JTong <eldertong@gmail.com>
2016-10-18 10:57:03 -0400

    what is the cats name?

---
+++ animal_sounds_test.rb
@@ -0,0 +1,16 @@
+# classes are just a blueprint
+
+require "minitest/autorun"
+require_relative "cat.rb"
+require_relative "dog.rb"
+require_relative "fox.rb"
+
+class TestAnimalSounds < Minitest::Test
+    
+    def test_cat_name
+        animal = Cat.new("Kitty")
+        assert_equal("Kitty", animal.name)
+    end
+
+
+    end
