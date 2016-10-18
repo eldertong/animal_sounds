1dd389b6c4ef42a2939798da327916426393db13
JTong <eldertong@gmail.com>
2016-10-18 11:32:06 -0400

    names are passing

--- animal_sounds_test.rb
+++ animal_sounds_test.rb
@@ -11,6 +11,14 @@ class TestAnimalSounds < Minitest::Test
         animal = Cat.new("Kitty")
         assert_equal("Kitty", animal.name)
     end
+    def test_dog_name
+        animal = Dog.new("Rover")
+        assert_equal("Rover", animal.name)
+    end
+    def test_fox_name
+        animal = Fox.new("Trouble")
+        assert_equal("Trouble", animal.name)
+    end
 
 
 end
